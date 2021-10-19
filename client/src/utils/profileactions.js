769757ce686b4ff80748608e1c99fa9e29531fab
Abhishek Trivedi <abhishek.trivedi280200@gmail.com>
2021-10-19 22:26:08 +0530

    DoctorProfile&&profiledone

--- client/src/utils/profileactions.js
+++ client/src/utils/profileactions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 // Get current profile
+//patient
 export const getCurrentProfile = () => {
   return axios
     .get('http://localhost:5000/profile', {
@@ -29,5 +30,38 @@ export const createProfile = (profileData, history) => {
       },
     })
     .then((res) => history.push('/patient/profile'))
+    .catch((err) => console.log(err));
+};
+
+//doctor
+//get profile
+export const getCurrentProfileD = () => {
+  return axios
+    .get('http://localhost:5000/doctor/profile', {
+      headers: {
+        'x-access-token': localStorage.getItem('token'),
+      },
+    })
+    .then(
+      (res) => {
+        return res.data;
+      }
+      // dispatch({
+      //   payload: res.data,
+      // })
+    );
+};
+
+// Create Profile
+export const createProfileD = (profileData, history) => {
+  console.log(profileData);
+  axios
+    .post('http://localhost:5000/doctor/profile', profileData, {
+      headers: {
+        'Content-Type': 'application/json',
+        'x-access-token': localStorage.getItem('token'),
+      },
+    })
+    .then((res) => history.push('/doctor/profile'))
     .catch((err) => console.log(err));
 };
